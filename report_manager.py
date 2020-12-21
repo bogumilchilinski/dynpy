03c09ae160ea7efa0e9b9831a7540eeb11994d95
boggich <user@fa57a33d76fc>
2020-12-21 07:10:14 +0000

    update all

---
+++ report_manager.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+from pylatex import Document, Section, Subsection, Tabular, Math, TikZ, Axis, Plot, Figure , Alignat, Package,Quantity, Command
+from pylatex.section import Chapter
+from pylatex.utils import italic,NoEscape
+import os
