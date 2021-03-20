2377e9de8d9f53d8558efb0667737ad0594bc3d0
Ongoing Project <ongoing@cocalc.com>
2021-03-20 09:31:23 +0000

    SDOF system

---
+++ models/systems.py
@@ -0,0 +1,22 @@
+from sympy import *  
+
+from ..dynamics import LagrangesDynamicSystem, HarmonicOscillator
+
+from sympy.physics.mechanics import *
+from sympy.physics.vector import *
+
+class SDOFHarmonicOscillator(HarmonicOscillator):
+    """Ready to use sample Single Degree of Freedom System with mass on spring
+    
+    """
+    def __init__(self,qs , m=Symbol('m',positive=True), k=Symbol('k',positive=True), ivar=Symbol('t')):
+        
+        self.m = m
+        self.k = k
+        self.qs = qs
+        
+        self.mass = MaterialPoint(m,pos1=qs)
+        self.spring = Spring(k,pos1=qs)
+        system = self.mass + self.spring
+        
+        super().__init__(system)
