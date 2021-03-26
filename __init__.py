adfbc15e6257015754fff36f372d8c7dad39f632
bogumilchilinski <bogumil.chilinski@gmail.com>
2021-03-26 21:40:19 +0100

    inits restore - dynpy

--- __init__.py
+++ __init__.py
@@ -4,20 +4,22 @@ This module provides basic tools for dynamic analysis of mechanical systems
 
 import importlib
 
-import .dynamics as dyn
+import dynpy.dynamics as dyn
 importlib.reload(dyn)
 
-import .solvers.linear as lin
-importlib.reload(lin)
-import .solvers.nonlinear as nonl
-importlib.reload(nonl)
-import .solvers.numerical as num
-importlib.reload(num)
+from .dynamics import *
 
-import .models.elements as elem
-importlib.reload(elem)
-import .models.systems as sys
-importlib.reload(sys)
+# import dynpy.solvers.linear as lin
+# importlib.reload(lin)
+# import dynpy.solvers.nonlinear as nonl
+# importlib.reload(nonl)
+# import dynpy.solvers.numerical as num
+# importlib.reload(num)
 
-import .utilities.timesiries as ts
-importlib.reload(ts)
+# import dynpy.models.elements as elem
+# importlib.reload(elem)
+# import dynpy.models.systems as sys
+# importlib.reload(sys)
+
+# import dynpy.utilities.timeseries as ts
+# importlib.reload(ts)
--- solvers/__init__.py
+++ solvers/__init__.py
@@ -5,8 +5,8 @@ This module provides basic tools for dynamic analysis of mechanical systems
 
 import importlib
 
-import .dynamics as dyn
-importlib.reload(dyn)
+# import .dynamics as dyn
+# importlib.reload(dyn)
 
-import .utilities.timesiries as ts
-importlib.reload(ts)
+# import .utilities.timesiries as ts
+# importlib.reload(ts)
