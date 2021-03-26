266b7dd71d289f214540b88efd0dd0bd5812f2e7
Ongoing Project <ongoing@cocalc.com>
2021-03-26 17:05:57 +0000

    Added reload to inits

--- __init__.py
+++ __init__.py
@@ -2,4 +2,22 @@
 This module provides basic tools for dynamic analysis of mechanical systems
 """
 
-from .dynamics import *
+import importlib
+
+import .dynamics as dyn
+importlib.reload(dyn)
+
+import .solvers.linear as lin
+importlib.reload(lin)
+import .solvers.nonlinear as nonl
+importlib.reload(nonl)
+import .solvers.numerical as num
+importlib.reload(num)
+
+import .models.elements as elem
+importlib.reload(elem)
+import .models.systems as sys
+importlib.reload(sys)
+
+import .utilities.timesiries as ts
+importlib.reload(ts)
