be29a885fcf09e598880e29c3598596ee2ec17d6
Ongoing Project <ongoing@cocalc.com>
2021-03-27 15:06:43 +0000

    bogumilchilinski/dynpy

--- __init__.py
+++ __init__.py
@@ -2,15 +2,15 @@
 This module provides basic tools for dynamic analysis of mechanical systems
 """
 
-<<<<<<< HEAD
+#<<<<<<< HEAD
 #import importlib
 
 #import dynpy.dynamics as dyn
 #importlib.reload(dyn)
 
 #from .dynamics import *
-=======
+#=======
 from .dynamics import *
->>>>>>> 34dc8c1df5540cc8ddc64108d2ba7892848df69f
+#>>>>>>> 34dc8c1df5540cc8ddc64108d2ba7892848df69f
 
 
