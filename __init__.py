82d7db60cbbd025c1cfd6c3e91fa8549c3144505
bogumilchilinski <bogumil.chilinski@gmail.com>
2021-03-28 23:49:49 +0200

    init crash update

--- __init__.py
+++ __init__.py
@@ -2,20 +2,7 @@
 This module provides basic tools for dynamic analysis of mechanical systems
 """
 
-#<<<<<<< HEAD
-#import importlib
-
-<<<<<<< HEAD
-# import dynpy.dynamics as dyn
-# importlib.reload(dyn)
-=======
-#import dynpy.dynamics as dyn
-#importlib.reload(dyn)
->>>>>>> be29a885fcf09e598880e29c3598596ee2ec17d6
-
-#from .dynamics import *
-#=======
 from .dynamics import *
-#>>>>>>> 34dc8c1df5540cc8ddc64108d2ba7892848df69f
+
 
 
