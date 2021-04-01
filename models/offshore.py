4cf4887e8be6935abcb91012d3b46cefde3ba9cd
bogumilchilinski <bogumil.chilinski@gmail.com>
2021-04-02 01:08:36 +0200

    offshore update

--- models/offshore.py
+++ models/offshore.py
@@ -1,11 +1,11 @@
 from .systems import ComposedSystem
 from sympy.physics.mechanics import dynamicsymbols
-from sympy import (Symbol, symbols, Matrix,sin,cos)
+from sympy import (Symbol, symbols, Matrix, sin, cos)
 
 
 class DDoFVessel(ComposedSystem):
     def __init__(self,
-                 m_vessel, 
+                 m_vessel,
                  I_5,
                  qs=dynamicsymbols('H, Phi'),
                  wave_level=dynamicsymbols('W'),
@@ -24,23 +24,31 @@ class DDoFVessel(ComposedSystem):
 
         K_matrix = Matrix([[rho*g*A_wl, -rho*g*A_wl*(CoF - CoB)],
                           [-rho*g*A_wl*(CoF - CoB), rho*g*V*GM_L]])
-        
-        #generalized displacements and velocities
-        q=Matrix(qs)+Matrix([wave_level,wave_slope])
-        dq=q.diff(self.ivar)
-        
+
+        # generalized displacements and velocities
+        q = Matrix(qs)+Matrix([wave_level, wave_slope])
+        dq = q.diff(self.ivar)
+
         # lagrangian components definition
         self.T = 1/2 * sum(dq.T * M_matrix * dq)
-        self.V = 1/2 * sum(Matrix(qs).T * K_matrix *  Matrix(qs))
+        self.V = 1/2 * sum(Matrix(qs).T * K_matrix * Matrix(qs))
 
-
-        super().__init__(Lagrangian=self.T-self.V,qs=qs)
+        super().__init__(Lagrangian=self.T-self.V, qs=qs)
 
 
 class TDoFCompensatedPayload(ComposedSystem):
 
-    def __init__(self,qs):
-        self.T = 1/2*m_p*v**2 + 1/2*m_c*v_c**2
-        self.V = 1/2*k_w*(h_c+h_c_eq)**2 + 1/2*k_c*(h+h_eq-(h_c+h_c_eq))**2 - m_p*g*z - m_c*g*z_c 
-        super().__init__(self.T-self.V,qs=qs)
+    def __init__(self,
+                 m_p=Symbol('m_p'),
+                 k_w=Symbol('k_w'),
+                 l_0=Symbol('l_0'), 
+                 qs=dynamicsymbols('varphi h h_c'),
+                                  m_c=Symbol('m_c'),
 
+                 ):
+
+
+        self.T = 1/2*m_p*v**2 + 1/2*m_c*v_c**2
+        self.V = 1/2*k_w*(h_c+h_c_eq)**2 + 1/2*k_c * \
+            (h+h_eq-(h_c+h_c_eq))**2 - m_p*g*z - m_c*g*z_c
+        super().__init__(self.T-self.V, qs=qs)
