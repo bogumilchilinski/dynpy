482b922a8e3e9aa7ecad844bcad2770222a698d3
Daniel <Daniel@example.com>
2021-04-04 20:55:55 +0200

    initt

--- stock_app/app_0/models.py
+++ stock_app/app_0/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+class Ticker(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
--- stock_app/stock_app/settings.py
+++ stock_app/stock_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app_0.apps.IdeasConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
