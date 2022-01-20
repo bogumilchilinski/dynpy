a95f3ab73b9ee5bac6913bf6250df0d9e726a3bf
Ongoing Project <ongoing@cocalc.com>
2022-01-20 16:23:18 +0000

    Daily upload - dynpy

--- utilities/templates/document.py
+++ utilities/templates/document.py
@@ -1,4 +1,4 @@
-from pylatex import (Document, Package,
+from pylatex import (Document, Package, Command
                      #Section, Subsection, Subsubsection, Itemize,  HorizontalSpace, Description, Marker
                     )
 #from pylatex.section import Paragraph, Chapter
@@ -6,71 +6,59 @@ from pylatex.utils import (#italic,
                            NoEscape)
 
 
-class TikzCaSCStandalone(Document):
+
+
+
+class CaseTemplate(Document):
+
     latex_name = 'document'
     packages = [
-        Package('natbib', options=['numbers']),
-
+                  Package('microtype'),
+                  Package('polski',options=['MeX']),
+                  Package('geometry',options=['lmargin=25mm', 'rmargin=25mm',  'top=30mm', 'bmargin=25mm', 'headheight=50mm']),
+                  Package('listings'),
+                  Package('titlesec'),
+                  Package('fancyhdr'),
+                  Command('pagestyle', arguments=['fancy']),
+                  Command('fancyhf', arguments=['']),
+                  Command('fancyhead',  arguments=['B. Chiliński, A. Mackojć'],options=['R']),
+                  Command('fancyhead', arguments=['Mechanical Vibration, 2021'],options=['L']),
+                  Command('fancyfoot', arguments=[NoEscape('\\thepage')],options=['C']),
+                  #Command('newcommand{\praca}', arguments=['Praca dyplomowa']),
+                  #Command('newcommand{\dyplom}', arguments=['Inżynierska']),
+                  #Command('newcommand{\kierunek}', arguments=['Wpisać kierunek']),
+                  #Command('newcommand{\specjalnosc}', arguments=['Wpisać specjalność']),
+                  #Command('newcommand{\\autor}', arguments=['Imię i nazwisko autora']),
+                  #Command('newcommand{\opiekun}', arguments=['Wpisać opiekuna']),
+                  #Command('newcommand{\promotor}', arguments=['Wpisać promotora']),
+                  #Command('newcommand{\konsultant}', arguments=['Wpisać konsultanta']),
+                  #Command('newcommand{\\tytul}', arguments=['Wpisać tytuł pracy dyplomowej po polsku']),
+                  #Command('newcommand{\\title}', arguments=['Wpisać tytuł pracy dyplomowej po angielsku']),
+                  #Command('newcommand{\supervisor}', arguments=['dr inż. Bogumił Chiliński']),
+                  #Command('newcommand{\\rok}', arguments=['Rok składania pracy']),
+                  #Command('newcommand{\kluczowe}', arguments=['Słowa kluczowe: Wpisać słowa kluczowe po polsku']),
+                  #Command('newcommand{\keywords}', arguments=['Keywords: Wpisać słowa kluczowe po angielsku']),
     ]
 
+
+# \renewcommand{\headrulewidth}{1pt}
+# \renewcommand{\footrulewidth}{1pt}
+    
+    
     def __init__(self,
                  default_filepath='default_filepath',
                  *,
-                 documentclass='standalone',
-                 document_options=NoEscape('tikz,class=cas-sc'),
+                 documentclass='article',
+                 document_options=None,
                  fontenc='T1',
                  inputenc='utf8',
                  font_size='normalsize',
                  lmodern=False,
                  textcomp=True,
-                 microtype=None,
+                 microtype=True,
                  page_numbers=True,
                  indent=None,
-                 geometry_options=None,
-                 data=None):
-
-        super().__init__(
-            default_filepath=default_filepath,
-            documentclass=documentclass,
-            document_options=document_options,
-            fontenc=fontenc,
-            inputenc=inputenc,
-            font_size=font_size,
-            lmodern=lmodern,
-            textcomp=textcomp,
-            microtype=microtype,
-            page_numbers=page_numbers,
-            indent=indent,
-            geometry_options=geometry_options,
-            data=data,
-        )
-
-
-class TikzStandalone(Document):
-    latex_name = 'document'
-    packages = [
-        #         Package('siunitx'),
-        #         Package('amsmath'),
-        #         Package('float'),
-        #         Package('tikz'),
-        #         Package('pgfplots'),
-        #         Command('usepgfplotslibrary', arguments='units'),
-        #         Command('usetikzlibrary', arguments='spy')
-    ]
-
-    def __init__(self,
-                 default_filepath='default_filepath',
-                 documentclass='standalone',
-                 document_options=[NoEscape('tikz')],
-                 fontenc='T1',
-                 inputenc='utf8',
-                 font_size='normalsize',
-                 lmodern=True,
-                 textcomp=True,
-                 microtype=None,
-                 page_numbers=True,
-                 indent=None,
-                 geometry_options=None,
+                 geometry_options=['lmargin=25mm', 'rmargin=25mm',  'top=20mm', 'bmargin=25mm', 'headheight=50mm'],
                  data=None):
 
         super().__init__(
