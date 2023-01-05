974e85572eb8f05e9810bf670cf354e20284eff7
bogumilchilinski <bogumil.chilinski@gmail.com>
2023-01-05 16:28:19 +0000

    On branch master Your branch is up to date with 'origin/master'.
    Daily update
    
    Changes to be committed:
    	modified:   __init__.py
    	modified:   solvers/linear.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 """
-This module provides basic tools for dynamic analysis of mechanical systems
+This module provides basic tools for dynamic analysis of mechanical systems - it's change to generate synchronization conflict
 """
 
 from .dynamics import (LagrangesDynamicSystem, LinearDynamicSystem,
