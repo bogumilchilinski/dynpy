8e317b264436a821ce8f4512356140a9af2afb2d
Filipluke <85788015+Filipluke@users.noreply.github.com>
2025-08-01 21:44:28 +0200

    AddedTestSchemeElectricModel

--- models/electric/circuits.py
+++ models/electric/circuits.py
@@ -83,3 +83,42 @@ class CircuitRLC(BaseCircuit):
             "ind": Inductor(L=self.L, q=q, ivar=self.ivar),
             "cap": Capacitor(C=self.C, q=q, ivar=self.ivar),
         }
+
+
+from dynpy.utilities.report import TikZPicture
+
+
+class ElecricScheme(TikZPicture):
+    def _scheme_desc(self):
+        return r"""
+\begin{scope}[american, every node/.style={font=\small}, line width=0.6pt]
+  % --- bloki logiczne -------------------------------------------------
+  \tikzset{block/.style={
+      draw, rounded corners, minimum width=3cm, minimum height=1.2cm,
+      font=\sffamily\small
+  }}
+  \node[block] (rpi) at (0,0)               {RPi\,4};
+  \node[block, right=3.5cm of rpi] (stm)    {RPI PICO};
+
+  % --- linia UART -----------------------------------------------------
+  \draw (rpi.east) -- node[above]{UART} (stm.west);
+
+  % --- linia PWM z R₂ przed silnikiem --------------------------------
+  \coordinate (pwm) at ($(stm.east)+(0.9,0)$);
+  \draw (stm.east) -- node[above]{GP26} (pwm)
+        to[R,l=$R_2$,*-*] ++(2,0) coordinate (motin);
+
+  % --- blok silnika DC -----------------------------------------------
+  \node[block, right=0.8cm of motin] (drv) {DC Motor};
+  \draw (motin) -- (drv.west);
+
+  % --- rezystor R₁ do masy -------------------------------------------
+  \draw (pwm) to[R,l=$R_1$] ++(0,-2) node[ground]{};
+
+  % --- wyłącznik oraz bateria ----------------------------------------
+  \draw (drv.east) -- ++(1.2,0)
+        to[spst,l=$S$,*-*] ++(1.5,0)
+        coordinate (bat+) |- ++(0,-3)
+        to[battery1,l=18650] ++(0,-2) node[ground]{};
+\end{scope}
+"""
