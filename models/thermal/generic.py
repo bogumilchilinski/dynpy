511f628a9b71616c3c32a8c2eb178ee6e2c40c3a
bogumilchilinski <bogumil.chilinski@gmail.com>
2026-08-03 00:50:46 +0200

    Implementation of `InertialThermalSystem` class that represents inertial thermodynamic system (within #1194 issue)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   models/thermal/generic.py - `InertialThermalSystem` class implemented.

--- models/thermal/generic.py
+++ models/thermal/generic.py
@@ -37,6 +37,7 @@ from ...dynamics import (
     base_origin,
 )
 from ..elements import (
+    MaterialPoint,
     # DerivativeElement,
     Force,
     # IntegralElement,
@@ -54,4 +55,50 @@ from ..mechanics.trolley import (
 
 #1194
 class InertialThermalSystem(ComposedSystem):
-    pass
+    m = Symbol("m", positive=True)
+    g = Symbol("g", positive=True)
+    c = Symbol("c", positive=True)
+    x = dynamicsymbols("x", positive=True)
+    y = dynamicsymbols("y", positive=True)
+    c0 = Symbol("c0", positive=True)
+    ivar = Symbol("t")
+
+    def __init__(self, m=None, g=None, c=None, x=None, y=None, ivar=None, **kwargs):
+
+        if m is not None:
+            self.m = m
+        if g is not None:
+            self.g = g
+        if c is not None:
+            self.c = c
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+        if ivar is not None:
+            self.ivar = ivar
+
+        self.qs = [self.x, self.y]
+
+        self._init_from_components(**kwargs)
+
+    @property
+    def components(self):
+
+        v_x = (self.x).diff(self.ivar)
+        v_y = (self.y).diff(self.ivar)
+        components = {}
+
+        self._missile_airless = MaterialPoint(
+            self.m, x=self.x, y=self.y, qs=self.qs
+        )
+
+        self._drag_x = Damper(self.c, self.x, qs=self.qs)(label="horizontal drag")
+
+        self._drag_y = Damper(self.c, self.y, qs=self.qs)(label="drag")
+
+        components["_missile_airless"] = self._missile_airless
+        components["_drag_x"] = self._drag_x
+        components["_drag_y"] = self._drag_y
+
+        return components
